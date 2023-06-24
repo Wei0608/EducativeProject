4a5cc3773b6590e911a647b5ffa7021bc133f692
Wei <redhairjake37@gmail.com>
2023-06-24 11:49:14 +0800

    add Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'reverse-phrase';
+  text = '';
+  reversedText = '';
+
+  onInputText(event: Event) {
+    this.text = (<HTMLInputElement>event.target).value;
+  }
+
+  onClickReverse() {
+    this.reversedText = this.text.split('').reverse().join('');
+  }
+
+  disableButton(): boolean {
+    return !this.text.length;
+  }
 }
